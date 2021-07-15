487cf4351b00920c929ae0d14c250447a337fb83
Amanda Southworth <amanda@withastra.com>
2021-07-15 15:08:17 -0700

    Creating basic export structure for lib files

---
+++ Codebase/Aureus-Package/lib/aureus.dart
@@ -0,0 +1,14 @@
+library aureus;
+
+//Exporting main dart stylization file
+export 'package:flutter/cupertino.dart';
+
+//Exporting base / prod variables
+export 'foundation.dart';
+export 'product.dart';
+
+//Exporting lib elements
+
+//Exporting lib components
+
+//Exporting lib views
---
+++ Codebase/Aureus-Package/lib/product.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/cupertino.dart/';
+
+//Contains all variables for use with the other components that are product specific.
+//Doc Link:
+
+class productVariations {
+
+
+
+}
