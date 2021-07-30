caaeea50dcc73ed4d7224490cef402dd37b412e0
Ingrid Wang <ingridwang@Ingrids-MacBook-Pro.local>
2021-07-29 21:03:54 -0400

    done?

--- Codebase/Aureus-Package/lib/Elements/Info Hiearchy/InfoHiearchyElements.dart
+++ Codebase/Aureus-Package/lib/Elements/Info Hiearchy/InfoHiearchyElements.dart
@@ -5,36 +5,32 @@ import 'package:aureus/foundation.dart';
 //A set of texts / label styles that acts as the typographic foundation for the rest of the UDS
 //Doc Link:
 
+UDSVariables udsVariables = new UDSVariables();
+
 class HeadingOneText extends Text {
   HeadingOneText(String data) : super(data);
-
 }
 
 class HeadingTwoText extends Text {
-  HeadingTwoText(String data) : super(data);
+  HeadingTwoText(String data) : super(data, style: udsVariables.heading2());
 }
 
 class HeadingThreeText extends Text {
-  HeadingThreeText(String data) : super(data);
-
+  HeadingThreeText(String data) : super(data, style: udsVariables.heading3());
 }
 
 class HeadingFourText extends Text {
-  HeadingFourText(String data) : super(data);
-
+  HeadingFourText(String data) : super(data, style: udsVariables.heading4());
 }
 
 class BodyOneText extends Text {
-  BodyOneText(String data) : super(data);
-
+  BodyOneText(String data) : super(data, style: udsVariables.body1());
 }
 
 class BodyTwoText extends Text {
-  BodyTwoText(String data) : super(data);
-
+  BodyTwoText(String data) : super(data, style: udsVariables.body2());
 }
 
 class SubheaderText extends Text {
-  SubheaderText(String data) : super(data);
-
+  SubheaderText(String data) : super(data, style: udsVariables.subheading());
 }
