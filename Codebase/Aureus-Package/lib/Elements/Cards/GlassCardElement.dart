8af4604739af2285b1b4705ca4fcfd84701a0343
Ingrid Wang <ingridwang@Ingrids-MacBook-Pro.local>
2021-08-04 21:09:01 -0400

    cards done

--- Codebase/Aureus-Package/lib/Elements/Cards/GlassCardElement.dart
+++ Codebase/Aureus-Package/lib/Elements/Cards/GlassCardElement.dart
@@ -4,6 +4,19 @@ import 'package:flutter/cupertino.dart';
 //Doc Link: https://github.com/Astra-Labs/Aureus/blob/main/Documentation/Aureus-Docs/4%20-%20Elements%20(Materials)/Cards/Glass%20Cards.md
 
 class GlassCardElement extends StatelessWidget {
+  BoxDecoration _createBoxDecoration(List<Color> colorsArray) {
+    return BoxDecoration(
+      borderRadius: BorderRadius.all(
+        Radius.circular(27),
+      ),
+      gradient: LinearGradient(
+        begin: Alignment.topLeft,
+        end: Alignment.bottomRight,
+        colors: colorsArray,
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -13,41 +26,22 @@ class GlassCardElement extends StatelessWidget {
             child: Container(
               height: 192,
               width: 189,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.all(
-                  Radius.circular(27),
-                ),
-                gradient: LinearGradient(
-                  begin: Alignment.topLeft,
-                  end: Alignment.bottomRight,
-                  colors: [
-                    Color.fromRGBO(255, 255, 255, 1),
-                    Color.fromRGBO(255, 255, 255, 0.17),
-                  ],
-                ),
-              ),
+              decoration: _createBoxDecoration([
+                Color.fromRGBO(255, 255, 255, 1),
+                Color.fromRGBO(255, 255, 255, 0.17),
+              ]),
             ),
           ),
           Positioned(
             child: Padding(
               padding: const EdgeInsets.all(1.0),
               child: Container(
-                height: 190,
-                width: 187,
-                decoration: BoxDecoration(
-                  gradient: LinearGradient(
-                    begin: Alignment.topLeft,
-                    end: Alignment.bottomRight,
-                    colors: [
-                      Color.fromRGBO(255, 255, 255, 0.15),
-                      Color.fromRGBO(255, 255, 255, 0),
-                    ],
-                  ),
-                  borderRadius: BorderRadius.all(
-                    Radius.circular(27),
-                  ),
-                ),
-              ),
+                  height: 190,
+                  width: 187,
+                  decoration: _createBoxDecoration([
+                    Color.fromRGBO(255, 255, 255, 0.15),
+                    Color.fromRGBO(255, 255, 255, 0),
+                  ])),
             ),
           ),
         ],
--- Codebase/Aureus-Package/lib/Elements/Cards/PastelCardElement.dart
+++ Codebase/Aureus-Package/lib/Elements/Cards/PastelCardElement.dart
@@ -1,9 +1,10 @@
+import 'package:aureus/Elements/Info%20Hiearchy/InfoHiearchyElements.dart';
 import 'package:aureus/Elements/Misc/TitleCase.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:aureus/foundation.dart';
 import 'package:flutter/material.dart';
 
-//A blank card that looks like a "pane" of glass
+//A blank card that is a pastel version of the product color
 //Doc Link: https://github.com/Astra-Labs/Aureus/blob/main/Documentation/Aureus-Docs/4%20-%20Elements%20(Materials)/Cards/Pastel%20Cards.md
 
 final foundation = UDSVariables();
@@ -28,13 +29,8 @@ class PastelCardElement extends StatelessWidget {
         ),
       ),
       child: Padding(
-        padding: const EdgeInsets.only(left: 13, top: 32),
-        child: Text(
-          titleCaseCardLabel,
-          style: foundation.body1().copyWith(
-                color: Colors.black,
-              ), //TODO need to change to info hierarchy when it makes it in to master
-        ),
+        padding: const EdgeInsets.only(left: 13, top: 35),
+        child: BodyOneText(titleCaseCardLabel, Colors.black),
       ),
     );
   }
