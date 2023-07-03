804fc6e28f847654adb7aa9b66cffeb9a4e926c2
Amanda Southworth <amanda@withastra.com>
2023-07-03 14:59:04 -0700

    Added border on focus bug fix

--- Package/lib/src/Widgets/Elements/Misc/InteractiveSemanticsWrapper.dart
+++ Package/lib/src/Widgets/Elements/Misc/InteractiveSemanticsWrapper.dart
@@ -1,4 +1,5 @@
 /// @nodoc
+import 'package:aureus/aureus.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 
@@ -6,25 +7,63 @@ import 'package:flutter/rendering.dart';
 /// {@subCategory Elements}
 /// {@image <image alt='' src=''>}
 
-/*--------- INTERACTIVE SEMANTICS ----------*/
+/*---------    SEMANTICS ----------*/
 /// A semantics wrapper that provides a Focus node for screen readers
 /// and tab orders to be established. This is just a little helper wrapper
 /// to combine Focus / semantics into one overarching wrapper class.
 
-class InteractiveSemanticsWrapper extends StatelessWidget {
+class InteractiveSemanticsWrapper extends StatefulWidget {
   /// The item that will have a 'floating glass' look behind it.
   final SemanticsProperties properties;
   final Widget child;
 
-  const InteractiveSemanticsWrapper(
-      {required this.properties, required this.child});
+  const InteractiveSemanticsWrapper({
+    required this.properties,
+    required this.child,
+  });
+
+  @override
+  _InteractiveSemanticsWrapperState createState() =>
+      _InteractiveSemanticsWrapperState();
+}
+
+class _InteractiveSemanticsWrapperState
+    extends State<InteractiveSemanticsWrapper> {
+  Color borderColor = Colors.transparent;
+
+  void hideBorder() {
+    setState(() {
+      borderColor = Colors.transparent.withAlpha(0);
+    });
+  }
+
+  void showBorder() {
+    setState(() {
+      borderColor = coloration.accentColor();
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Focus(
-      child: Semantics.fromProperties(
-        properties: properties,
-        child: child,
+    return Container(
+      padding: const EdgeInsets.all(2.0),
+      decoration: BoxDecoration(
+        border: Border.all(
+          color: borderColor,
+        ),
+      ),
+      child: Focus(
+        onFocusChange: (focused) {
+          if (focused == true) {
+            showBorder();
+          } else {
+            hideBorder();
+          }
+        },
+        child: Semantics.fromProperties(
+          properties: widget.properties,
+          child: widget.child,
+        ),
       ),
     );
   }
